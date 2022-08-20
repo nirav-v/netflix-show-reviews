b1b8eaba83d826734884eae3de629a230ed4fb97
Nirav Venkatesan <niravvenkatesan@gmail.com>
2022-08-20 08:17:27 -0700

    added addtional cloumns to the Movie model and added netflix csv file

--- app/models/Movie.py
+++ app/models/Movie.py
@@ -7,8 +7,13 @@ from sqlalchemy.orm import relationship, column_property
 class Movie(Base):
     __tablename__ = 'movies'
     id = Column(Integer, primary_key=True)
-    type = Column(String(100), nullable=False)
+    type = Column(String(100))
     title = Column(String(100), nullable=False)
+    director = Column(String(100))
+    country = Column(String(100))
     release_year = Column(Integer, nullable=False)
+    rating = Column(String(55))
+    duration = Column(String(55))
+    description = Column(String(255))
 
     reviews = relationship('Review', cascade='all,delete')
