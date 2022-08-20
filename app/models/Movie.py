a0f45e713167339e3bffad988e30dd74c05aaef2
Nirav Venkatesan <niravvenkatesan@gmail.com>
2022-08-20 12:11:49 -0700

    working code for seeding movies db with director and title columns for each movie. Used pandas to read in the csv file as a dataframe (like an object) . Had to loop through every row in table (using len of the movie titles since only the column headers were accesible as properties) passing in values for each field using the corresponding index of each fields array.

--- app/models/Movie.py
+++ app/models/Movie.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from app.db import Base
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, select, func
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, select, func, true
 from sqlalchemy.orm import relationship, column_property
 
 
@@ -8,10 +8,12 @@ class Movie(Base):
     __tablename__ = 'movies'
     id = Column(Integer, primary_key=True)
     type = Column(String(100))
-    title = Column(String(100), nullable=False)
-    director = Column(String(100))
+    title = Column(String(2000))
+    director = Column(String(2000))
+    cast = Column(String(255))
     country = Column(String(100))
-    release_year = Column(Integer, nullable=False)
+    date_added = Column(String(100))
+    release_year = Column(Integer)
     rating = Column(String(55))
     duration = Column(String(55))
     description = Column(String(255))
--- seeds.py
+++ seeds.py
@@ -1,3 +1,13 @@
+from numpy import genfromtxt
+import pandas as pd
+
+from time import time
+from datetime import datetime
+from sqlalchemy import Column, Integer, Float, Date
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 from app.models import User, Post, Comment, Vote, Movie, Review
 from app.db import Session, Base, engine
 
@@ -7,55 +17,37 @@ Base.metadata.create_all(engine)
 
 db = Session()
 
+# insert csv data into movies
+# function from stack overflow answer
+
+movies = pd.read_csv('netflix_titles.csv', index_col=0,
+                     quotechar='"', delimiter=",")
+
+df = movies
+# code from stack overflow to convert 'nan' values into None to prevent sql error
+df.dropna(inplace=True)
+
+
+df1 = df.where((pd.notnull(df)), None)
+
+for i in range(len(movies.title)):
+    # director = movies.director[d]
+    # print(director)
+
+    db.add_all([Movie(title=movies.title[i], director=movies.director[i])])
+
+
 # insert users
-db.add_all([
-    User(username='alesmonde0', email='valid@bgf.abg', password='password123'),
-    User(username='jwilloughway1',
-         email='rmebes1@sogou.com', password='password123'),
-    User(username='iboddam2', email='cstoneman2@last.fm', password='password123'),
-    User(username='dstanmer3', email='ihellier3@goo.ne.jp', password='password123'),
-    User(username='djiri4', email='gmidgley4@weather.com', password='password123')
-])
+# db.add_all([
+#     User(username='alesmonde0', email='valid@bgf.abg', password='password123'),
+#     User(username='jwilloughway1',
+#          email='rmebes1@sogou.com', password='password123'),
+#     User(username='iboddam2', email='cstoneman2@last.fm', password='password123'),
+#     User(username='dstanmer3', email='ihellier3@goo.ne.jp', password='password123'),
+#     User(username='djiri4', email='gmidgley4@weather.com', password='password123')
+# ])
 
 db.commit()
 
-# # insert posts
-# db.add_all([
-#     Post(title='Donec posuere metus vitae ipsum',
-#          post_url='https://buzzfeed.com/in/imperdiet/et/commodo/vulputate.png', user_id=1),
-#     Post(title='Morbi non quam nec dui luctus rutrum',
-#          post_url='https://nasa.gov/donec.json', user_id=1),
-#     Post(title='Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue',
-#          post_url='https://europa.eu/parturient/montes/nascetur/ridiculus/mus/etiam/vel.aspx', user_id=2),
-#     Post(title='Nunc purus', post_url='http://desdev.cn/enim/blandit/mi.jpg', user_id=3),
-#     Post(title='Pellentesque eget nunc',
-#          post_url='http://google.ca/nam/nulla/integer.aspx', user_id=4)
-# ])
-
-# db.commit()
-
-# # insert comments
-# db.add_all([
-#     Comment(comment_text='Nunc rhoncus dui vel sem.', user_id=1, post_id=2),
-#     Comment(comment_text='Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.', user_id=1, post_id=3),
-#     Comment(comment_text='Aliquam erat volutpat. In congue.',
-#             user_id=2, post_id=1),
-#     Comment(comment_text='Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.',
-#             user_id=2, post_id=3),
-#     Comment(comment_text='In hac habitasse platea dictumst.', user_id=3, post_id=3)
-# ])
-
-# db.commit()
-
-# # insert votes
-# db.add_all([
-#     Vote(user_id=1, post_id=2),
-#     Vote(user_id=1, post_id=4),
-#     Vote(user_id=2, post_id=4),
-#     Vote(user_id=3, post_id=4),
-#     Vote(user_id=4, post_id=2)
-# ])
-
-# db.commit()
 
 db.close()
