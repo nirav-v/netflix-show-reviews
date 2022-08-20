17e550893522e1025c72076276e12b8a1388301a
Nirav Venkatesan <niravvenkatesan@gmail.com>
2022-08-20 12:19:32 -0700

    successfully seed movies db with all desired data from netflix csv file

--- seeds.py
+++ seeds.py
@@ -1,6 +1,5 @@
 from numpy import genfromtxt
 import pandas as pd
-
 from time import time
 from datetime import datetime
 from sqlalchemy import Column, Integer, Float, Date
@@ -24,17 +23,16 @@ movies = pd.read_csv('netflix_titles.csv', index_col=0,
                      quotechar='"', delimiter=",")
 
 df = movies
-# code from stack overflow to convert 'nan' values into None to prevent sql error
+# code from stack overflow to convert 'nan' values into None to prevent sql error when inserting an empty entry
 df.dropna(inplace=True)
-
 
 df1 = df.where((pd.notnull(df)), None)
 
 for i in range(len(movies.title)):
-    # director = movies.director[d]
-    # print(director)
-
-    db.add_all([Movie(title=movies.title[i], director=movies.director[i])])
+    db.add_all([
+        Movie(type=movies.type[i], title=movies.title[i],
+               director=movies.director[i], cast=movies.cast[i], country=movies.country[i], date_added=movies.date_added[i], release_year=movies.release_year[i], rating=movies.rating[i], duration=movies.duration[i], description=movies.description[i])
+    ])
 
 
 # insert users
