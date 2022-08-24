de82ce46828cfa260f0cb2e5be0dfa130b5b31f5
Nirav Venkatesan <niravvenkatesan@gmail.com>
2022-08-24 15:46:25 -0700

    re-added venv

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/niravvenkatesan/post-bootcamp-projects/python-newsfeed/venv"
+set -gx VIRTUAL_ENV "/Users/niravvenkatesan/post-bootcamp-projects/netflix-show-reviews/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
